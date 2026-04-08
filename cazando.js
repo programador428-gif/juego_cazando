b2a334d3d627a953d2726a7207e0f179506ec23f
Dilan Solorzano <230013094+programador428-gif@users.noreply.github.com>
2026-04-08 17:12:51 -0500

    Variables defined on the Cat and the Food, their positions modified accordingly to what the exercise requires

--- cazando.js
+++ cazando.js
@@ -1,11 +1,23 @@
 function iniciarJuego() {
     let canvas = document.getElementById("areaJuego");
     let ctx = canvas.getContext("2d");
-
+    const ALTO_GATO = 25;
+    const ANCHO_GATO = 50;
+    const ALTO_COMIDA = 20;
+    const ANCHO_COMIDA = 20;
+    let gatoY = canvas.height / 2 - ALTO_GATO;
+    let gatoX = canvas.width / 2 - ANCHO_GATO / 2;
+    let comidaY = canvas.height - ALTO_COMIDA;
+    let comidaX = canvas.width - ANCHO_COMIDA;
     function graficarGato() {
         ctx.fillStyle = "black";
-        ctx.fillRect((canvas.width / 2) - 50, (canvas.height / 2) - 25, 100, 50);
+        ctx.fillRect(gatoX, gatoY, ANCHO_GATO, ALTO_GATO);
     }
 
+    function graficarComida() {
+        ctx.fillStyle = "red";
+        ctx.fillRect(comidaX, comidaY, ANCHO_COMIDA, ALTO_COMIDA);
+    }
     graficarGato();
+    graficarComida();
 }
