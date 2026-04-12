5a55c32aa210f9a7cf27d390f1b763bb85bde7f2
Dilan Solorzano <230013094+programador428-gif@users.noreply.github.com>
2026-04-11 20:38:47 -0500

    mover izquierda integrado

--- cazando.js
+++ cazando.js
@@ -1,29 +1,40 @@
+let canvas = document.getElementById("areaJuego");
+let ctx = canvas.getContext("2d");
+const ALTO_GATO = 50;
+const ANCHO_GATO = 50;
+const ALTO_COMIDA = 20;
+const ANCHO_COMIDA = 20;
+let gatoY = canvas.height / 2 - ALTO_GATO;
+let gatoX = canvas.width / 2 - ANCHO_GATO / 2;
+let comidaY = canvas.height - ALTO_COMIDA;
+let comidaX = canvas.width - ANCHO_COMIDA;
+
+// Funciones
+
+function graficarRectangulo(x, y, ancho, alto, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, ancho, alto);
+}
+
+function graficarGato() {
+    graficarRectangulo(gatoX, gatoY, ANCHO_GATO, ALTO_GATO, "black");
+}
+
+function graficarComida() {
+    graficarRectangulo(comidaX, comidaY, ANCHO_COMIDA, ALTO_COMIDA, "red");
+}
+
+function limpiarCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function moverIzquierda() {
+    gatoX -= 10;
+    limpiarCanvas();
+    graficarGato();
+}
+
 function iniciarJuego() {
-    let canvas = document.getElementById("areaJuego");
-    let ctx = canvas.getContext("2d");
-    const ALTO_GATO = 25;
-    const ANCHO_GATO = 50;
-    const ALTO_COMIDA = 20;
-    const ANCHO_COMIDA = 20;
-    let gatoY = canvas.height / 2 - ALTO_GATO;
-    let gatoX = canvas.width / 2 - ANCHO_GATO / 2;
-    let comidaY = canvas.height - ALTO_COMIDA;
-    let comidaX = canvas.width - ANCHO_COMIDA;
-
-    // Funciones
-
-    function graficarRectangulo(x, y, ancho, alto, color) {
-        ctx.fillStyle = color;
-        ctx.fillRect(x, y, ancho, alto);
-    }
-
-    function graficarGato() {
-        graficarRectangulo(gatoX, gatoY, ANCHO_GATO, ALTO_GATO, "black");
-    }
-
-    function graficarComida() {
-        graficarRectangulo(comidaX, comidaY, ANCHO_COMIDA, ALTO_COMIDA, "red");
-    }
     graficarGato();
     graficarComida();
 }
